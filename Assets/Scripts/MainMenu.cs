22298abd40865fdc98c9a673d8638f369531323e
olarif <76481740+olarif@users.noreply.github.com>
2022-02-27 12:25:49 +0100

    last stuff

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,42 +8,10 @@ using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
-    [Header("Volume Settings")]
-    [SerializeField] private TMP_Text volumeTextValue;
-    [SerializeField] private Slider volumeSlider;
-    [SerializeField] private float defaultVolume = 1f;
 
-    private void Start()
-    {
-        volumeTextValue.text = PlayerPrefs.GetFloat("masterVolume").ToString("0.0");
-        volumeSlider.value = PlayerPrefs.GetFloat("masterVolume");
-    }
-
-    
-    public void SetVolume(float volume)
-    {
-        AudioListener.volume = volume;
-        volumeTextValue.text = volume.ToString("0.0");
-    }
-
-    public void Apply()
-    {
-        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
-    }
-
-
-    public void ResetButton()
-    {
-        AudioListener.volume = defaultVolume;
-        volumeSlider.value = defaultVolume;
-        volumeTextValue.text = defaultVolume.ToString("0.0");
-
-        Apply();
-    }
-    
     public void PlayGame()
     {
-        SceneManager.LoadScene("NameSelect");
+        SceneManager.LoadScene("MegaScene");
     }
 
     public void QuitGame()
